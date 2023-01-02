a02d6f6ae74ff70f251275980f3068f06e25030a
Sejal Verma <sejal.verma@encoresky.com>
2023-01-02 17:40:25 +0530

    Added color and background color

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const generator = (img, props) => {
   if (props.value) {
-    img.src = `https://api.qrserver.com/v1/create-qr-code/?data=${props.value}&amp;size=${props.size}`;
+    img.src = `https://api.qrserver.com/v1/create-qr-code/?data=${props.value}&size=${props.size}&color=${props.color}&bgcolor=${props.bgcolor}`;
     img.alt = props.qrAlt;
     img.title = props.title;
   }
@@ -11,11 +11,13 @@ exports.qrcodeGenerator = ({
   size = "180x180",
   qrAlt = "React easy QR image",
   title = "React easy QR generator",
+  bgcolor = "fff",
+  color = "000",
   showQrId,
 }) => {
   let img = document.createElement("img");
   let qr = document.getElementById(showQrId);
   qr.appendChild(img);
 
-  generator(img, { value, size, qrAlt, title });
+  generator(img, { value, size, qrAlt, title, color, bgcolor});
 };
